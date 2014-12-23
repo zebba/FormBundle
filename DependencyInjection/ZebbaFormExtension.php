134534583aa84c95f26c750c7f88f635e3bdef1c
Sebastian <zebba@hotmail.de>
2014-12-23 23:53:45 +0100

    Modified dependencies

--- DependencyInjection/ZebbaFormExtension.php
+++ DependencyInjection/ZebbaFormExtension.php
@@ -23,9 +23,6 @@ class ZebbaFormExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-
-        if (isset($config['DoctrineBundle'])) {
-            $loader->load('services.xml');
-        }
+		$loader->load('services.xml');
     }
 }
